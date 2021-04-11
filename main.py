c7a657c83635f1c108f61729501cd9817c1fa0b4
Elypha <i@elypha.com>
2021-04-11 21:00:21 +0900

    minor update

--- main.py
+++ main.py
@@ -9,25 +9,26 @@ import tokyohot
 
 if __name__ == '__main__':
 
+    # conf
     root_raw = R'K:\0'
     root_finished = R'K:\1'
+    single_processe = 0
+    processes = 12
 
+    # main
     root_raw = root_raw.replace('\\', '/')
     root_finished = root_finished.replace('\\', '/')
     videos = JK.file.findall(root_raw, ['mp4', 'mkv', 'wmv', 'avi'])
 
     print(F'Find {len(videos)} files\n\n')
 
-    # main
-    p = Pool(12)
-
-    for video in videos:
-        p.apply_async(dmm.scraper, args=(video, root_finished, ))
-
-    p.close()
-    p.join()
-
-    # main (debug)
-    # for video in videos:
-    #     print(video)
-    #     dmm.scraper(video, root_finished)
+    if single_processe == 0:
+        p = Pool(processes)
+        for video in videos:
+            p.apply_async(dmm.scraper, args=(video, root_finished, ))
+        p.close()
+        p.join()
+    else:
+        for video in videos:
+            print(video)
+            dmm.scraper(video, root_finished)
